a1ec67d7ff44d24cc7ed799905496b90915ce58d
Nawinav <avnaveenkumar16@gmail.com>
2020-02-21 12:39:34 +0530

    Implemented Generic repository

--- MyShopCart/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShopCart/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -15,12 +15,12 @@ namespace MyShop.WebUI.Controllers
     public class ProductManagerController : Controller
     {
         // GET: ProductManager
-        ProductRepository context;
-        ProductCategoryRepository productCategories;
+        InMemoryRepository<Product> context;
+        InMemoryRepository<ProductCategory> productCategories;
         public ProductManagerController()
         {
-            context=new ProductRepository();
-            productCategories = new ProductCategoryRepository();
+            context= new InMemoryRepository<Product>();
+            productCategories = new InMemoryRepository<ProductCategory>();
         }
         public ActionResult Index()
             
